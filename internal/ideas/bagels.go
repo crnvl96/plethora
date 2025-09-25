2ae0a478b4f40005007b4560fe602bf68ad732fb
Adran Carnavale <adran@hey.com>
2025-09-25 20:57:39 -0300

    add bagels

--- internal/ideas/bagels.go
+++ internal/ideas/bagels.go
@@ -2,11 +2,23 @@ package ideas
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sort"
+	"strconv"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/crnvl96/plethora/internal/ui"
+)
+
+const (
+	numDigits  = 3
+	maxGuesses = 10
 )
 
 type bagels struct {
@@ -22,13 +34,78 @@ func (b *bagels) SetStderr(w io.Writer) { b.stderr = w }
 func (b *bagels) Run() error {
 	scanner := bufio.NewScanner(b.stdin)
 
-	fmt.Fprintln(b.stdout, "Bagels!")
-	fmt.Fprint(b.stdout, "Enter your name: ")
+	// Handle Ctrl+C to quit gracefully
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	go func() {
+		<-sigChan
+		fmt.Fprintln(b.stderr, "\nInterrupted. Thanks for playing!")
+		os.Exit(0)
+	}()
 
-	if scanner.Scan() {
-		name := scanner.Text()
-		fmt.Fprintf(b.stdout, "Hello, %s!\n", name)
+	fmt.Fprintf(b.stdout, `Bagels, a deductive login game.
+	By Al Sweigart al@inventwithpython.com
+
+	I am thinking of a %d-digit number with no repeated digits.
+	Try to guess what it is. Here are some clues:
+
+	When I say:     That means:
+	Pico			One digit is correct but in the wrong position
+		Fermi			One digit is correct and in the right position
+	Bagels			No digit is correct
+
+	For example, if the secret number was 248 and your guess was 843, the clues would be Fermi Pico.
+	`, numDigits)
+
+	for {
+		secretNum := getSecretNum()
+		fmt.Fprintln(b.stdout, "I have thought up a number")
+		fmt.Fprintf(b.stdout, "You have %d guesses to get it\n", maxGuesses)
+
+		var numGuesses int
+		for numGuesses = 1; numGuesses <= maxGuesses; numGuesses++ {
+
+			guess := ""
+
+			for {
+				fmt.Fprintf(b.stdout, "Guess #%d: ", numGuesses)
+				if !scanner.Scan() {
+					return errors.New("failed to read input")
+				}
+				guess = strings.TrimSpace(scanner.Text())
+				if len(guess) == numDigits {
+					if _, err := strconv.Atoi(guess); err == nil {
+						break
+					}
+				}
+			}
+
+			clues := getClues(guess, secretNum)
+			fmt.Fprintln(b.stdout, clues)
+
+			if guess == secretNum {
+				break
+			}
+		}
+
+		if numGuesses > maxGuesses {
+			fmt.Fprintln(b.stdout, "You ran out of guesses")
+			fmt.Fprintf(b.stdout, "The answer was %s\n", secretNum)
+		}
+
+		fmt.Fprintln(b.stdout, "Want to play again? (y/n)")
+		if !scanner.Scan() {
+			break
+		}
+		answer := strings.TrimSpace(strings.ToLower(scanner.Text()))
+
+		if !strings.HasPrefix(answer, "y") {
+			break
+		}
+
 	}
+
+	fmt.Fprintln(b.stdout, "Thanks for playing")
 
 	return nil
 }
@@ -43,5 +120,50 @@ func init() {
 		Body:     "Bagels is a deductive logic game. You must guess a secret three-digit number based on clues.",
 		Command:  &bagels{},
 		Callback: onErr,
+	}
+}
+
+func getSecretNum() string {
+	numbers := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+	rand.Shuffle(len(numbers), func(i, j int) {
+		numbers[i], numbers[j] = numbers[j], numbers[i]
+	})
+
+	return strings.Join(numbers[:numDigits], "")
+}
+
+func getClues(guess, secret string) string {
+	if guess == secret {
+		return "You got it!"
+	}
+
+	clues := []string{}
+	tempSecret := []rune(secret)
+
+	for i := 0; i < len(guess); i++ {
+		if guess[i] == secret[i] {
+			clues = append(clues, "Fermi")
+			tempSecret[i] = ' '
+		}
+	}
+
+	for i := 0; i < len(guess); i++ {
+		if guess[i] != secret[i] {
+			for j, s := range tempSecret {
+				if s == rune(guess[i]) {
+					clues = append(clues, "Pico")
+					tempSecret[j] = ' '
+					break
+				}
+			}
+		}
+	}
+
+	if len(clues) == 0 {
+		return "Bagels"
+	} else {
+		sort.Strings(clues)
+		return strings.Join(clues, " ")
 	}
 }
